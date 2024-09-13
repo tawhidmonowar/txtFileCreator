ecbd01809cc3139d517a9be975b0d72d5097f940
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-09-14 03:11:05 +0600

    Update NavigationBars.kt

--- app/src/main/java/com/tawhid/txtfilecreator/NavigationBars.kt
+++ app/src/main/java/com/tawhid/txtfilecreator/NavigationBars.kt
@@ -1,9 +1,14 @@
 package com.tawhid.txtfilecreator
 
 import android.annotation.SuppressLint
+import android.transition.Scene
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
@@ -43,6 +48,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 data class BottomNavigationItem(
     val title: String,
@@ -72,7 +80,7 @@ fun NavigationBars() {
             badgeCount = 99
         ),
         BottomNavigationItem(
-            title = "Pins",
+            title = ".txtPins",
             selectedIcon = painterResource(id = R.drawable.pin_filled),
             unselectedIcon = painterResource(id = R.drawable.pin_outline),
             hasNews = true,
@@ -81,6 +89,8 @@ fun NavigationBars() {
     var selectedItemIndex by rememberSaveable {
         mutableStateOf(0)
     }
+
+    val navigationController = rememberNavController()
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -128,6 +138,11 @@ fun NavigationBars() {
                             selected = selectedItemIndex == index,
                             onClick = {
                                 selectedItemIndex = index
+                                when (index) {
+                                    0 -> navigationController.navigate(Screens.Editor.screen) {popUpTo(0)}
+                                    1 -> navigationController.navigate(Screens.Files.screen) {popUpTo(0)}
+                                    2 -> navigationController.navigate(Screens.Pins.screen) {popUpTo(0)}
+                                }
                             },
                             label = {
                                 Text(text = item.title)
@@ -137,7 +152,7 @@ fun NavigationBars() {
                                     badge = {
                                         if (item.badgeCount != null) {
                                             Badge {
-                                                Text(text = item.badgeCount.toString()+"+")
+                                                Text(text = item.badgeCount.toString() + "+")
                                             }
                                         } else if (item.hasNews) {
                                             Badge()
@@ -159,19 +174,14 @@ fun NavigationBars() {
             },
 
             ) { values ->
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(values)
+            NavHost(
+                navController = navigationController,
+                startDestination = Screens.Editor.screen,
+                modifier = Modifier.padding(values)
             ) {
-                items(100) { it ->
-                    Text(
-                        modifier = Modifier.padding(15.dp),
-                        text = "item $it",
-                        fontSize = 18.sp
-                    )
-                    Divider()
-                }
+                composable(Screens.Editor.screen) { Editor() }
+                composable(Screens.Files.screen) { Files() }
+                composable(Screens.Pins.screen) { Pins() }
             }
         }
     }
