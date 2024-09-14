4e6dcc68d019f7a09fd7f15f57e154f2ea79c573
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-09-14 15:18:43 +0600

    Update Theme.kt

--- app/src/main/java/com/tawhid/txtfilecreator/ui/theme/Theme.kt
+++ app/src/main/java/com/tawhid/txtfilecreator/ui/theme/Theme.kt
@@ -10,49 +10,86 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+
+    primary = Dark,
+    onPrimary = Light,
+
+    primaryContainer = Red40,
+    onPrimaryContainer = Green90,
+
+    inversePrimary = Green40,
+
+    secondary = Dark,
+    onSecondary = Red40,
+    secondaryContainer = Red40,
+    onSecondaryContainer = DarkGreen90,
+    tertiary = Violet80,
+    onTertiary = Violet20,
+    tertiaryContainer = Violet30,
+    onTertiaryContainer = Violet90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+
+    background = Dark,
+    onBackground = Light,
+
+    surface = Gray12,
+    onSurface = Light,
+    inverseSurface = Red40,
+    inverseOnSurface = Grey10,
+    surfaceVariant = Gray12,
+    onSurfaceVariant = Light,
+    outline = GreenGrey80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
+    primary = Light,
+    onPrimary = Color.DarkGray,
+    primaryContainer = Green90,
+    onPrimaryContainer = Green10,
+    inversePrimary = Green80,
+    secondary = DarkGreen40,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondaryContainer = DarkGreen90,
+    onSecondaryContainer = DarkGreen10,
+    tertiary = Violet40,
+    onTertiary = Color.DarkGray,
+    tertiaryContainer = Violet90,
+    onTertiaryContainer = Violet10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = GreenGrey90,
+    onSurface = GreenGrey30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = GreenGrey90,
+    onSurfaceVariant = GreenGrey30,
+    outline = GreenGrey50
 )
 
 @Composable
 fun TxtFileCreatorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
@@ -61,7 +98,6 @@ fun TxtFileCreatorTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
-
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
