91c00662968c09d0b33401240bca1e9a5c5b2d9d
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-10-20 13:37:26 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("androidx.room") version "2.6.1" apply false
 }
 
 buildscript {
